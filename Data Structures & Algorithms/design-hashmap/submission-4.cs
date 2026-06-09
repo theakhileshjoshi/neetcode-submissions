3dd728a7b06f15633e01b80913afb8b57d902971
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2026-06-10 00:37:50 +0530

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.cs
@@ -0,0 +1,26 @@
+public class MyHashMap {
+    private int[] arr = Enumerable.Repeat(-1, 1000001).ToArray();
+    public MyHashMap() {
+        
+    }
+    
+    public void Put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    public int Get(int key) {
+        return arr[key];
+    }
+    
+    public void Remove(int key) {
+        arr[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.Put(key,value);
+ * int param_2 = obj.Get(key);
+ * obj.Remove(key);
+ */
