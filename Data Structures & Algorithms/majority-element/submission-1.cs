eafa58abeda4a5e6b1ea631edcbab7fd9b2d8496
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2026-06-10 00:11:46 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        return nums.GroupBy(i => i)
+            .Where(i => i.Count() > nums.Length/2)
+            .Select(i => i.Key)
+            .First();
+    }
+}
